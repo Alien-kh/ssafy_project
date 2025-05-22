ab2cfcbb065ae6e90e86f15438c6b8de3cd67b55
alien <ghwh323232@naver.com>
2025-05-22 15:47:54 +0900

    test

---
+++ django-pjt/diaries/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'diaries'
+
+urlpatterns = [
+    # 여기에 필요한 URL 패턴을 추가하세요
+    # 예: path('', views.diary_list, name='diary_list'),
+]
--- django-pjt/my_api/urls.py
+++ django-pjt/my_api/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('api/v1/recommendations', include('recommendations.urls')),
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls')),
+    
 ]
